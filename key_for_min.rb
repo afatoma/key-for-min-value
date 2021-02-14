f5e2ee6a50cd0fb68d0673986b59d3bed92632f4
Amos Fatoma <amosfatoma@gmail.com>
2021-02-14 15:29:07 -0600

    commit tree 22edefcc68821d3fee313c2e6e5893a88c49409c

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+boxer = {:lightweight => 135, :middleweight =>165, :heavyweight => 205}
 def key_for_min_value(name_hash)
+    lowest_key = nil
+  lowest_value = Float::INFINITY
+  name_hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+
 
 end
